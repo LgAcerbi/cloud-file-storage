1e16aaf8a5f41c042bafe7d57ce8824f5f99d121
lgacerbi <lgacerbi@gmail.com>
2026-03-30 23:11:29 -0300

    feat: update FileRemoteGateway trait to include file upload and update methods

--- apps/sync-agent/src/application/ports/file_remote_gateway.rs
+++ apps/sync-agent/src/application/ports/file_remote_gateway.rs
@@ -1,5 +1,4 @@
-use crate::domain::entities::file_metadata::FileMetadata;
-
 pub trait FileRemoteGateway {
-    fn get_file_metadata_by_id(&self, id: &str) -> Option<FileMetadata>;
+    fn upload_file(&self, file_blob: Vec<u8>);
+    fn update_file(&self, id: &str, file_blob: Vec<u8>);
 }
